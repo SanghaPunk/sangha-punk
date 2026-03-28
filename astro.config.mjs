85215da54dc5d3b536cf6e69fb66cf81c0515ac1
Jessica Pla <130689381+jessicapla@users.noreply.github.com>
2026-03-28 18:30:31 -0300

    ci: criar workflow de deploy para staging com SST (#21)
    * chore: downgrade astro to v5
    
    * chore: install sst
    
    * chore: set aws provider in sst config
    
    * ci: add deploy to staging workflow
    
    * fix(ci): use dev branch for deploy to staging

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 // @ts-check
 
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from "astro/config"
-import react from "@astrojs/react"
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import aws from "astro-sst";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
+  vite: {
+    // @ts-ignore
     plugins: [tailwindcss()],
   },
   integrations: [react()],
-})
-
+  output: "server",
+  adapter: aws(),
+});
