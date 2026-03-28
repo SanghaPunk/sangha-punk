3e3ceef22397709521259b6d0410c65646a96370
Jessica Pla <jessicab.pla@gmail.com>
2026-03-28 18:23:26 -0300

    chore: set aws provider in sst config

--- sst.config.ts
+++ sst.config.ts
@@ -7,9 +7,14 @@ export default $config({
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
+      providers: {
+        aws: {
+          profile: input?.stage === "staging" ? "staging" : "default",
+        }
+      }
     };
   },
   async run() {
-    new sst.aws.Astro("MyWeb");
+    new sst.aws.Astro("SanghaPunk");
   },
 });
